a9bbec9663bb9e3408261f78b9f059d98e689dea
liujie <liu_anxin@163.com>
2019-08-21 15:36:20 +0800

    fix rebuild

--- src/main/java/com/github/liuanxin/caches/MybatisRedisCache.java
+++ src/main/java/com/github/liuanxin/caches/MybatisRedisCache.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.Arrays;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.regex.Pattern;
@@ -15,8 +16,6 @@ import java.util.regex.Pattern;
  * usr redis cache in mybatis, use RedisTemplate in spring context
  *
  * 1. add com.github.liuanxin.caches.RedisContextUtils in spring context
- * 2. add &lt;cache type="com.github.liuanxin.caches.MybatisRedisCache" /&gt; in mapper.xml.
- *
  * &#064;Configuration
  * public class MybatisCacheConfig {
  *
@@ -25,16 +24,23 @@ import java.util.regex.Pattern;
  *     return new com.github.liuanxin.caches.RedisContextUtils();
  *   }
  * }
+ *
+ * 2. add &lt;cache type="com.github.liuanxin.caches.MybatisRedisCache" /&gt; in mapper.xml.
  * </pre>
  */
 public class MybatisRedisCache implements Cache {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MybatisRedisCache.class);
 
+    private static final Pattern COMMA_REGEX = Pattern.compile(", ");
+    private static final String COMMA = ",";
     private static final Pattern BLANK_REGEX = Pattern.compile("\\s{2,}");
-    private static final String SPACE = " ";
+    private static final String BLANK = " ";
+
+    private static final byte[] NIL_CACHE = "".getBytes();
 
     private static RedisTemplate<Object, Object> redisTemplate;
+
 
     private final String id;
     private final ReadWriteLock readWriteLock;
@@ -43,8 +49,17 @@ public class MybatisRedisCache implements Cache {
         if (id == null) {
             throw new IllegalArgumentException("Cache instances require an ID");
         }
-        this.id = BLANK_REGEX.matcher(id).replaceAll(SPACE);
+        this.id = handleBlank(id);
         this.readWriteLock = new ReentrantReadWriteLock();
+    }
+
+    private String handleBlank(String str) {
+        // <multi space> replace to <one space>
+        // <, >          replace to <,>
+        String s = str;
+        s = BLANK_REGEX.matcher(s).replaceAll(BLANK);
+        s = COMMA_REGEX.matcher(s).replaceAll(COMMA);
+        return s;
     }
 
     @Override
@@ -55,29 +70,30 @@ public class MybatisRedisCache implements Cache {
     @Override
     public int getSize() {
         RedisTemplate<Object, Object> redisTemplate = getRedis();
-        if (redisTemplate == null) {
-            return 0;
-        }
-        try {
-            return redisTemplate.opsForHash().size(id.getBytes()).intValue();
-        } catch (RedisConnectionFailureException e) {
-            if (LOGGER.isWarnEnabled()) {
-                LOGGER.warn(e.getMessage());
+        if (redisTemplate != null) {
+            try {
+                return redisTemplate.opsForHash().size(id.getBytes()).intValue();
+            } catch (RedisConnectionFailureException e) {
+                if (LOGGER.isWarnEnabled()) {
+                    LOGGER.warn(e.getMessage());
+                }
             }
-            return 0;
         }
+        return 0;
     }
 
     @Override
     public void putObject(final Object key, final Object value) {
-        if (key != null && value != null) {
+        if (key != null) {
             RedisTemplate<Object, Object> redisTemplate = getRedis();
             if (redisTemplate != null) {
-                String keyHash = BLANK_REGEX.matcher(key.toString()).replaceAll(SPACE);
+                String k = handleBlank(key.toString());
                 try {
-                    redisTemplate.opsForHash().put(id.getBytes(), keyHash.getBytes(), SerializeUtil.serialize(value));
+                    // avoid cache penetration
+                    byte[] v = (value == null) ? NIL_CACHE : JdkSerializer.serialize(value);
+                    redisTemplate.opsForHash().put(id.getBytes(), k.getBytes(), v);
                     if (LOGGER.isDebugEnabled()) {
-                        LOGGER.debug("put query result ({}) to cache", (id + "<>" + keyHash));
+                        LOGGER.debug("put query result ({}) to cache", (id + "<>" + k));
                     }
                 } catch (RedisConnectionFailureException e) {
                     if (LOGGER.isWarnEnabled()) {
@@ -90,26 +106,30 @@ public class MybatisRedisCache implements Cache {
 
     @Override
     public Object getObject(final Object key) {
-        if (key == null) {
-            return null;
-        }
-        RedisTemplate<Object, Object> redisTemplate = getRedis();
-        if (redisTemplate != null) {
-            String keyHash = BLANK_REGEX.matcher(key.toString()).replaceAll(SPACE);
-            try {
-                Object value = redisTemplate.opsForHash().get(id.getBytes(), keyHash.getBytes());
-                if (value != null && value instanceof byte[]) {
-                    Object result = SerializeUtil.unSerialize((byte[]) value);
-                    if (result != null) {
-                        if (LOGGER.isDebugEnabled()) {
-                            LOGGER.debug("get query result ({}) from cache", (id + "<>" + keyHash));
+        if (key != null) {
+            RedisTemplate<Object, Object> redisTemplate = getRedis();
+            if (redisTemplate != null) {
+                String k = handleBlank(key.toString());
+                try {
+                    Object value = redisTemplate.opsForHash().get(id.getBytes(), k.getBytes());
+                    if (value instanceof byte[]) {
+                        byte[] bytes = (byte[]) value;
+                        // avoid cache penetration
+                        if (Arrays.equals(NIL_CACHE , bytes)) {
+                            return null;
                         }
-                        return result;
+                        Object result = JdkSerializer.unSerialize(bytes);
+                        if (result != null) {
+                            if (LOGGER.isDebugEnabled()) {
+                                LOGGER.debug("get query result ({}) from cache", (id + "<>" + k));
+                            }
+                            return result;
+                        }
                     }
-                }
-            } catch (RedisConnectionFailureException e) {
-                if (LOGGER.isWarnEnabled()) {
-                    LOGGER.warn(e.getMessage());
+                } catch (RedisConnectionFailureException e) {
+                    if (LOGGER.isWarnEnabled()) {
+                        LOGGER.warn(e.getMessage());
+                    }
                 }
             }
         }
@@ -117,27 +137,26 @@ public class MybatisRedisCache implements Cache {
     }
 
     @Override
+    @SuppressWarnings("RedundantCast")
     public Object removeObject(final Object key) {
-        if (key == null) {
-            return null;
-        }
-        RedisTemplate<Object, Object> redisTemplate = getRedis();
-        if (redisTemplate == null) {
-            return null;
-        }
-        String keyHash = BLANK_REGEX.matcher(key.toString()).replaceAll(SPACE);
-        try {
-            Object obj = redisTemplate.opsForHash().delete(id.getBytes(), (Object) keyHash.getBytes());
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("remove query result ({}) from cache", (id + "<>" + keyHash));
+        if (key != null) {
+            RedisTemplate<Object, Object> redisTemplate = getRedis();
+            if (redisTemplate != null) {
+                String k = handleBlank(key.toString());
+                try {
+                    Object obj = redisTemplate.opsForHash().delete(id.getBytes(), (Object) k.getBytes());
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("remove query result ({}) from cache", (id + "<>" + k));
+                    }
+                    return obj;
+                } catch (RedisConnectionFailureException e) {
+                    if (LOGGER.isWarnEnabled()) {
+                        LOGGER.warn(e.getMessage());
+                    }
+                }
             }
-            return obj;
-        } catch (RedisConnectionFailureException e) {
-            if (LOGGER.isWarnEnabled()) {
-                LOGGER.warn(e.getMessage());
-            }
-            return null;
         }
+        return null;
     }
 
     @Override
